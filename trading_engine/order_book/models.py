5a9b28f6c1faae857bb6ec1834f4281f7b1b0d4d
Najib Oladosu <najiboladosu@gmail.com>
2024-03-04 08:00:49 +0100

    Adding the Asset, Order and OderBook classes

--- trading_engine/order_book/models.py
+++ trading_engine/order_book/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+# Asset basically mean each stock  or commodity that will be represented in the orderbook.
+class Asset(models.Model):
+    name = models.CharField(max_length=100)
+    ticker = models.CharField(max_length=10)
+    # other fields like description, etc.
+
+# The Order class defines the properties of a order and how it will be executed  by the matching engine.
+class Order(models.Model):
+    asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=15, decimal_places=2)
+    quantity = models.DecimalField(max_digits=15, decimal_places=2)
+    # other fields like user, timestamp, order type, etc.
+
+# This is the core of the trading engine and it symbolizes where all orders are executed.
+class OrderBook(models.Model):
+    asset = models.OneToOneField(Asset, on_delete=models.CASCADE)
+    # other fields like exchange, status, etc.
