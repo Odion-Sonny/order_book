b6b19a75c7f13189bad1d05ecca069aed3aa9bb7
NajibOladosu <najiboladosu@gmail.com>
2024-03-12 15:26:09 +0100

    Updated classes

--- trading_engine/order_book/models.py
+++ trading_engine/order_book/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -16,6 +17,7 @@ class Order(models.Model):
         ('MARKET', 'Market'),
         ('STOP_LOSS', 'Stop Loss'),
     ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=15, decimal_places=2)
     size = models.DecimalField(max_digits=15, decimal_places=2)
@@ -25,5 +27,6 @@ class Order(models.Model):
 
 # This is the core of the trading engine and it symbolizes where all orders are executed.
 class OrderBook(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
     asset = models.OneToOneField(Asset, on_delete=models.CASCADE)
     # other fields like exchange, status may be needed.
